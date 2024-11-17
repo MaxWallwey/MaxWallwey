f9535ff0b01f5165fdcd4b8392dab5771674be78
MaxWallwey <max.wallwey@gmail.com>
2024-11-17 18:32:05 +0000

    WIP

---
+++ portfolio/app/wedding/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WeddingOverview() {
+  return <h1>This is the wedding page!</h1>;
+}
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        offwhite: "#f1f1f1",
+        white: "#fff",
+        black: "#000000",
+        darkgray: "#121212",
+      },
+      fontFamily: {
+        sans: ['"PT Sans"', "sans-serif"],
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
