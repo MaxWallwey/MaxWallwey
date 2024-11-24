d8add88466f43153bb61c9d362f1d72824f89133
MaxWallwey <max.wallwey@gmail.com>
2024-11-24 16:24:56 +0000

    Navbar improvements

---
+++ portfolio/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from '../../src/components/Navbar';
+
+export default function About() {
+  return (
+    <>
+      <Navbar />
+      <h1>About me!</h1>
+    </>
+  );
+}
