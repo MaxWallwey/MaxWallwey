e51a8f9b74ec5649db504e6866e9164f1ac27ee0
MaxWallwey <max.wallwey@gmail.com>
2024-11-24 16:35:18 +0000

    WIP

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Navbar } from '../src/components/Navbar';
+import { Home } from '../src/components/Home';
 
-export default function Home() {
+export default function Homepage() {
   return (
     <>
       <header>
         <Navbar />
+        <Home />
       </header>
     </>
   );
