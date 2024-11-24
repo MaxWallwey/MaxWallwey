e3de1aa51d47ff8e8b0d35265cacd55dbc49c420
MaxWallwey <max.wallwey@gmail.com>
2024-11-24 16:25:05 +0000

    WIP

--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,18 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 export const Navbar: React.FC = () => {
+  const pathName = usePathname()
+  
   return (
     <div
-      style={{
-        fontSize: '20px',
-        fontWeight: 'bold',
-      }}
-      className='font-sans flex flex-row justify-between max-w-5xl mx-auto  px-4 py-3 sm:px-6'
+      className='flex justify-center mt-3 text-xl'
     >
-      <a href='/'>Home</a>
-      <div>
-        <a href='/wedding'>Wedding</a>
-        <a href='/me'>Me</a>
+      <div className='bg-white border-2 border-solid border-black rounded-full'>
+        <a className={pathName==='/' ? 'mx-3 font-semibold' : 'mx-3 hover:font-semibold'} href='/'>Home</a>
+        <a className={pathName==='/about' ? 'mx-3 font-semibold' : 'mx-3 hover:font-semibold'} href='/about'>About</a>
       </div>
     </div>
   );
