3dfd46ef9d1ae81cdfe7d48c1ee3a027c67db038
MaxWallwey <127151268+MaxWallwey@users.noreply.github.com>
2025-06-08 12:01:27 +0100

    WIP

--- portfolio/app/wedding/page.tsx
+++ portfolio/app/wedding/page.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '../../src/components/Navbar';
 
 export default function WeddingOverview() {
   return (
-    <body className="min-h-screen bg-white">
+    <body className="min-h-screen">
       <header className="sticky">
         <Navbar />
       </header>
--- portfolio/src/components/Wedding/Wedding.tsx
+++ portfolio/src/components/Wedding/Wedding.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image';
-import { portrait } from '/images/portrait.png';
 
 export const Wedding: React.FC = () => {
   return (
-    <Image
-      src={portrait}
-      width={500}
-      height={500}
-      alt="Portrait of the bride and groom"
-    />
+    <body>
+      <>
+        <Image src="/img/portrait.png" alt="Portrait of the bride and groom" />
+      </>
+    </body>
   );
 };
